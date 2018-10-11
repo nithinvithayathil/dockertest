98be5d101ee54c9d06a02f41f42732898534346c
nithinvithayathil <44019282+nithinvithayathil@users.noreply.github.com>
2018-10-11 15:08:47 +0530

    Add files via upload

---
+++ app/server.js
@@ -0,0 +1,47 @@
+exports.rgbToHex = function(red, green, blue) {
+
+  var redHex   = red.toString(16);
+  var greenHex = green.toString(16);
+  var blueHex  = blue.toString(16);
+
+  return pad(redHex) + pad(greenHex) + pad(blueHex);
+
+};
+
+function pad(hex) {
+  return (hex.length === 1 ? "0" + hex : hex);
+}
+exports.hexToRgb = function(hex) {
+
+  var red   = parseInt(hex.substring(0, 2), 16);
+  var green = parseInt(hex.substring(2, 4), 16);
+  var blue  = parseInt(hex.substring(4, 6), 16);
+
+  return [red, green, blue];
+
+};
+
+root@jenkins:/home/jenkins/oldfiles/converter/app# cat server.js
+var express = require("express");
+var app = express();
+var converter = require("./converter");
+
+app.get("/rgbToHex", function(req, res) {
+  var red   = parseInt(req.query.red, 10);
+  var green = parseInt(req.query.green, 10);
+  var blue  = parseInt(req.query.blue, 10);
+
+  var hex = converter.rgbToHex(red, green, blue);
+
+  res.send(hex);
+});
+
+app.get("/hexToRgb", function(req, res) {
+  var hex = req.query.hex;
+
+  var rgb = converter.hexToRgb(hex);
+
+  res.send(JSON.stringify(rgb));
+});
+
+app.listen(3000);
